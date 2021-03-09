ee47d2e8d35b8b926d336c9bc22d56c60dd7f26d
Christina Williams <christinaxtwilliams@gmail.com>
2021-03-09 14:22:21 -0500

    engine

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const { Engine, Render, Runner, World, Bodies } = Matter;
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
-
+  element: document.body,
+  engine: engine,
+  options: {
+    
+  }
 });
